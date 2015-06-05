894c66bb15622bbcf19359c3a11e1bb8e7b202cb
lexoyo <a.hoyau@silexlabs.org>
2015-06-05 10:36:00 +0200

    added github URL

--- js/script.js
+++ js/script.js
@@ -56,7 +56,7 @@ if(l.search.indexOf('utm_expid='+k)>0)return;
 function f(n){if(c){var i=c.indexOf(n+'=');if(i>-1){var j=c.
 indexOf(';',i);return escape(c.substring(i+n.length+1,j<0?c.
 length:j))}}}var x=f('__utmx'),xx=f('__utmxx'),h=l.hash;d.write(
-'<sc'+'ript http://preprod.silex.me/api/1.0/dropbox/exec/get/sites-silex/monitoshi/editable.htmlhttp://editor.silex.me/api/1.0/dropbox/exec/get/sites-silex/monitoshi/editable.htmlsrc="'+'http'+(l.protocol=='https:'?'s://ssl':
+'<sc'+'ript http://preprod.silex.me/api/1.0/dropbox/exec/get/sites-silex/monitoshi/editable.htmlhttp://editor.silex.me/api/1.0/dropbox/exec/get/sites-silex/monitoshi/editable.htmlhttp://editor.silex.me/api/1.0/dropbox/exec/get/sites-silex/monitoshi/editable.htmlhttp://editor.silex.me/api/1.0/dropbox/exec/get/sites-silex/monitoshi/editable.htmlsrc="'+'http'+(l.protocol=='https:'?'s://ssl':
 '://www')+'.google-analytics.com/ga_exp.js?'+'utmxkey='+k+
 '&utmx='+(x?x:'')+'&utmxx='+(xx?xx:'')+'&utmxtime='+new Date().
 valueOf()+(h?'&utmxhash='+escape(h.substr(1)):'')+
